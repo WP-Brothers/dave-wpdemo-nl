83271ff56ec78489f174d8676f35f1d715cbbfaf
davidsd29 <davidsdankwah@gmail.com>
2024-03-06 10:56:40 +0100

    related card styling and logics added

--- wp-content/themes/socialbrothers/library/woocommerce/archive.php
+++ wp-content/themes/socialbrothers/library/woocommerce/archive.php
@@ -1,8 +1,29 @@
 <?php
 
-add_action('woocommerce_before_shop_loop_item_title', 'wpb_product_meta_tag', 5);
+add_action('woocommerce_before_shop_loop_item_title', 'wpb_product_meta_tag', );
 function wpb_product_meta_tag() 
 {
     global $product;
-    echo wc_get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">' . _n( '', '', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</span>' ); 
+    $product_tags = wc_get_product_tag_list( $product->get_id(), ', ', '', '' );
+    if ( ! empty( $product_tags ) ) : ?>
+        <div class="tagged_as product-card__meta_tag">
+            <?= $product_tags;?>
+        </div>
+    <?php endif;
+}
+
+add_action('woocommerce_shop_loop_item_title', 'wpb_product_meta_category', 5);
+function wpb_product_meta_category() 
+{
+    global $product;
+
+    $product_cats = wc_get_product_category_list( $product->get_id(), ', ', '', '' );
+    if ( ! empty( $product_cats ) ) : ?>
+        <div class="product_meta">
+            <span class="posted_in">
+                <?php echo $product_cats ?>
+            </span>
+        </div>
+
+    <?php endif;
 }
